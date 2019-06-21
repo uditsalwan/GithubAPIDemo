7b9e8fd81df1bee9fdb5d0c8733179bbac9fc994
“Udit” <udit.salwan@gmail.com>
2019-06-21 10:51:12 +0530

    Initial Structure

---
+++ GithubIssueViewer/ListView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubIssueViewer
+//
+//  Created by Udit on 21/06/19.
+//  Copyright © 2019 iOSDemo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
